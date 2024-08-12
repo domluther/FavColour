9aa8c40c96aa728a40334d15bacfec0c31341396
dom <35171942+domluther@users.noreply.github.com>
2024-08-12 21:31:16 +0100

    separate repo

---
+++ backend.js
@@ -0,0 +1,144 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+
+app = express();
+app.use(morgan('tiny'));
+
+const PORT = process.env.PORT || 5432;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.set('view engine', 'ejs');
+
+let coll;
+
+async function connectToDB() {
+  try {
+    const URI = process.env.MONGO_URI;
+    const client = new MongoClient(URI);
+    const dbName = 'ColourVoter';
+    const collName = 'colours';
+    await client.connect();
+    coll = client.db(dbName).collection(collName);
+    console.log(`Connected to ${dbName}`);
+    return coll;
+  } catch (error) {
+    console.error(`Failed to connect to DB - ${error}`);
+    process.exit(1);
+  }
+}
+// Need to connect
+(async () => {
+  coll = await connectToDB();
+})();
+
+app.get('/', async (req, res) => {
+  try {
+    console.log(coll);
+    colours = await coll.find({}).sort({ votes: -1 }).toArray();
+    res.render('index.ejs', { colours });
+  } catch (error) {
+    console.error(`Failed to fetch homepage ${error}`);
+    res.status(500).send({ error: 'Failed to fetch homepage' });
+  }
+  // res.sendFile(Path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/api/colours/', async (req, res) => {
+  try {
+    console.log('Request for all the colours');
+    // I want them in vote order
+    colours = await coll.find({}).sort({ votes: 1 }).toArray();
+    res.send({ colours });
+  } catch (error) {
+    console.error(`Failed to fetch colours ${error}`);
+    res.status(500).send({ error: 'Failed to fetch colours' });
+  }
+});
+
+app.post('/api/colours/', async (req, res) => {
+  try {
+    // Read in new colour
+    const colour = req.body.colour;
+    // Colour blank?
+    if (colour === '') {
+      return res.status(400).send({ error: 'Empty body' });
+    }
+    // No colour property?
+    if (!colour) {
+      return res.status(400).send({ error: 'Malformed body' });
+    }
+    //   Is it already in the DB?
+    const existingColour = await coll.findOne({ name: colour });
+    if (existingColour) {
+      return res.status(400).send({ error: 'Duplicate colour' });
+    }
+
+    // Passed validation - make a new object
+    // Has 1 vote as creating classes as voting too
+    const newColour = {
+      name: colour,
+      votes: 1,
+    };
+
+    console.log(`Submitting ${JSON.stringify(newColour)} to DB`);
+    await coll.insertOne(newColour);
+    // Re-render page?
+    res.send({ message: 'success' });
+  } catch (error) {
+    console.error(`Failed to add colour ${error}`);
+    res.status(500).send({ error: 'Failed to add colour' });
+  }
+});
+
+app.put('/api/colours/:colour/:direction', async (req, res) => {
+  try {
+    const colour = req.params.colour;
+    const change = req.params.direction === 'up' ? 1 : -1;
+    // Increment vote count
+    const dbRes = await coll.updateOne({ name: colour }, [
+      {
+        $set: {
+          votes: {
+            $max: [{ $add: ['$votes', change] }, 0],
+          },
+        },
+      },
+    ]);
+    // Record didn't exist?
+    if (dbRes.matchedCount === 0) {
+      return res.status(400).send({ error: 'Invalid colour' });
+    }
+    res.send({ message: 'Success' });
+  } catch (error) {
+    console.error(`Failed to update colour ${error}`);
+    res.status(500).send({ error: 'Failed to update colour' });
+  }
+});
+
+app.delete('/api/colours/:colour', async (req, res) => {
+  try {
+    const colour = req.params.colour;
+
+    //   Send request to delete
+    const dbRes = await coll.deleteOne({ name: colour });
+
+    // Respond based on code - deleted v not
+    if (dbRes.deletedCount === 0) {
+      return res.status(400).send({ error: 'Not there to delete' });
+    }
+    //  Frontend reloads page
+    res.json({ colour, status: 'deleted' });
+  } catch (error) {
+    console.error(`Failed to delete colour ${error}`);
+    res.status(500).send({ error: 'Failed to delete colour' });
+  }
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
---
+++ public/main.js
@@ -0,0 +1,273 @@
+const submitBtn = document.querySelector('#colourForm button');
+const colourList = document.querySelector('.colourList');
+
+colourList.addEventListener('click', handleColourListClick);
+
+async function handleColourListClick(e) {
+  // Guard clauses
+  if (e.target.classList.contains('votes')) return;
+  if (e.target.classList.contains('colourName')) return;
+  if (e.target.classList.contains('colour')) return;
+  try {
+    const colour = e.target.ariaLabel.split(' ')[1];
+    if (e.target.classList.contains('delete')) {
+      handleDelete(colour);
+    }
+    if (e.target.classList.contains('upvote')) {
+      handleVote(colour, 'up');
+    }
+    if (e.target.classList.contains('downvote')) {
+      handleVote(colour, 'down');
+    }
+  } catch (error) {
+    const errorMsg = `Network error ${error}`;
+    console.error(errorMsg);
+    setNotification(errorMsg);
+  }
+}
+
+async function handleDelete(colour) {
+  const res = await fetch(`/api/colours/${colour}`, { method: 'DELETE' });
+  if (res.ok) {
+    setNotification(`Deleted ${colour}`, false);
+    // Lower server load than EJS
+    render();
+  } else {
+    const errorData = await res.json();
+    const errorMsg = `Error deleting ${colour} : ${errorData.error}`;
+    setNotification(errorMsg);
+  }
+}
+
+async function handleVote(colour, direction) {
+  const res = await fetch(`/api/colours/${colour}/${direction}`, {
+    method: 'PUT',
+  });
+  if (res.ok) {
+    // location.reload();
+    // Lower server load than EJS
+    render();
+    setNotification('Voted', false);
+  } else {
+    const errorData = await res.json();
+    const errorMsg = `Error upvoting ${colour} : ${errorData.error}`;
+    console.error(errorMsg);
+    setNotification(errorMsg);
+  }
+}
+
+// No need to handle the submit button, just use an action and render it - does refreshing however
+submitBtn.addEventListener('click', handleSubmit);
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  const colourInpEle = document.querySelector('#colourInput');
+  const colour = colourInpEle.value.toLowerCase();
+  const validColors = [
+    'aliceblue',
+    'antiquewhite',
+    'aqua',
+    'aquamarine',
+    'azure',
+    'beige',
+    'bisque',
+    'black',
+    'blanchedalmond',
+    'blue',
+    'blueviolet',
+    'brown',
+    'burlywood',
+    'cadetblue',
+    'chartreuse',
+    'chocolate',
+    'coral',
+    'cornflowerblue',
+    'cornsilk',
+    'crimson',
+    'cyan',
+    'darkblue',
+    'darkcyan',
+    'darkgoldenrod',
+    'darkgray',
+    'darkgreen',
+    'darkkhaki',
+    'darkmagenta',
+    'darkolivegreen',
+    'darkorange',
+    'darkorchid',
+    'darkred',
+    'darksalmon',
+    'darkseagreen',
+    'darkslateblue',
+    'darkslategray',
+    'darkturquoise',
+    'darkviolet',
+    'deeppink',
+    'deepskyblue',
+    'dimgray',
+    'dodgerblue',
+    'firebrick',
+    'floralwhite',
+    'forestgreen',
+    'fuchsia',
+    'gainsboro',
+    'ghostwhite',
+    'gold',
+    'goldenrod',
+    'gray',
+    'green',
+    'greenyellow',
+    'honeydew',
+    'hotpink',
+    'indianred',
+    'indigo',
+    'ivory',
+    'khaki',
+    'lavender',
+    'lavenderblush',
+    'lawngreen',
+    'lemonchiffon',
+    'lightblue',
+    'lightcoral',
+    'lightcyan',
+    'lightgoldenrodyellow',
+    'lightgray',
+    'lightgreen',
+    'lightpink',
+    'lightsalmon',
+    'lightseagreen',
+    'lightskyblue',
+    'lightslategray',
+    'lightsteelblue',
+    'lightyellow',
+    'lime',
+    'limegreen',
+    'linen',
+    'magenta',
+    'maroon',
+    'mediumaquamarine',
+    'mediumblue',
+    'mediumorchid',
+    'mediumpurple',
+    'mediumseagreen',
+    'mediumslateblue',
+    'mediumspringgreen',
+    'mediumturquoise',
+    'mediumvioletred',
+    'midnightblue',
+    'mintcream',
+    'mistyrose',
+    'moccasin',
+    'navajowhite',
+    'navy',
+    'oldlace',
+    'olive',
+    'olivedrab',
+    'orange',
+    'orangered',
+    'orchid',
+    'palegoldenrod',
+    'palegreen',
+    'paleturquoise',
+    'palevioletred',
+    'papayawhip',
+    'peachpuff',
+    'peru',
+    'pink',
+    'plum',
+    'powderblue',
+    'purple',
+    'red',
+    'rosybrown',
+    'royalblue',
+    'saddlebrown',
+    'salmon',
+    'sandybrown',
+    'seagreen',
+    'seashell',
+    'sienna',
+    'silver',
+    'skyblue',
+    'slateblue',
+    'slategray',
+    'snow',
+    'springgreen',
+    'steelblue',
+    'tan',
+    'teal',
+    'thistle',
+    'tomato',
+    'turquoise',
+    'violet',
+    'wheat',
+    'white',
+    'whitesmoke',
+    'yellow',
+    'yellowgreen',
+  ];
+
+  if (validColors.includes(colour)) {
+    const res = await fetch('/api/colours/', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({ colour }),
+    });
+    if (res.ok) {
+      setNotification(`Added ${colour}`, false);
+      colourInpEle.value = '';
+      render();
+    } else {
+      const data = await res.json();
+      const errorMsg = `${data.error}`;
+      console.error(errorMsg);
+      setNotification(errorMsg);
+    }
+  } else {
+    const errorMsg = `${colour} is not a valid HTML color name.`;
+    console.error(errorMsg);
+    setNotification(errorMsg);
+  }
+}
+
+function setNotification(msg, error = true) {
+  notificationEle = document.querySelector('.notification');
+  notificationEle.classList.remove('hidden');
+  notificationEle.innerText = msg;
+  if (error) {
+    notificationEle.classList.add('error');
+    notificationEle.classList.remove('info');
+  } else {
+    notificationEle.classList.add('info');
+    notificationEle.classList.remove('error');
+  }
+  // Make it disappear after 2500ms
+  setTimeout(() => notificationEle.classList.add('hidden'), 2500);
+}
+
+async function render() {
+  console.log('Rendering page');
+  const res = await fetch('/api/colours');
+  const data = await res.json();
+  const { colours } = data;
+  const colourList = document.querySelector('.colourList');
+  colourList.innerHTML = '';
+  colours.forEach((colour) => {
+    colourList.insertAdjacentHTML(
+      'afterbegin',
+      `<li class="colour" style="background-color:${colour.name};">
+            <span class="colourName">${colour.name}</span>
+            <span class="votes">${colour.votes}</span>
+            <button class="upvote" aria-label="Upvote ${colour.name}">👍</button>
+            <button class="downvote" aria-label="Upvote ${colour.name}">👎</button>
+            <button class="delete" aria-label="Delete ${colour.name}">🗑️</button>
+          </li>`
+    );
+  });
+}
+
+// Renders - can use instead of reloading
+// render();
+
+// setNotification();
